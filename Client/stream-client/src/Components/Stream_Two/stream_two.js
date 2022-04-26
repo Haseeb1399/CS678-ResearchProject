04a9a5e0a338f89b8f7cb7305296b4def0fd8849
Haseeb <haseeb1399@gmail.com>
2022-04-26 06:49:28 +0500

    added server url

--- Client/stream-client/src/Components/Stream_Two/stream_two.js
+++ Client/stream-client/src/Components/Stream_Two/stream_two.js
@@ -4,7 +4,7 @@ function StreamTwo() {
   return (
     <div>
        <h1>StreamOne</h1>
-        <ReactPlayer url='http://localhost:8000/live/stream_720.flv' playing={true} controls={true} width={1000} height={500}/>
+        <ReactPlayer url='http://139.59.159.233:8000/live/stream_720.flv' playing={true} controls={true} width={1000} height={500}/>
     </div>
   );
 }
