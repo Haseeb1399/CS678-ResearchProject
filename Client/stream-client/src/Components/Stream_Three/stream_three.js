621dfcbc7f345b4a8f16466625aa1e86353b7027
Haseeb <haseeb1399@gmail.com>
2022-04-26 11:47:15 +0500

    added new ssl urls

--- Client/stream-client/src/Components/Stream_Three/stream_three.js
+++ Client/stream-client/src/Components/Stream_Three/stream_three.js
@@ -4,7 +4,7 @@ function StreamThree() {
   return (
     <div>
        <h1>StreamOne</h1>
-        <ReactPlayer url='http://139.59.159.233:8000/live/stream_480.flv' playing={true} controls={true} width={1000} height={500}/>
+        <ReactPlayer url='https://topicsservercsproject.xyz/live/stream_480.flv' playing={true} controls={true} width={1000} height={500}/>
     </div>
   );
 }
